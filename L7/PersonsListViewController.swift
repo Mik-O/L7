2871a98a9628318546200b4532722a76582f2e6e
mikle <mikle.kontinent@gmail.com>
2025-06-14 20:06:26 +0700

    working with TableView

--- L7/PersonsListViewController.swift
+++ L7/PersonsListViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class PersonsListViewController: UITableViewController {
     var persons : [Person] = []
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        persons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellContact", for: indexPath)
+        
+        let person = persons[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = person.fullName
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 
 }
