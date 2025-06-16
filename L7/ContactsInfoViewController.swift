f178ff132cf7348a6c339ae5a1ef54fab58f0b0f
mikle <mikle.kontinent@gmail.com>
2025-06-16 16:08:05 +0700

    almost working version

--- L7/ContactsInfoViewController.swift
+++ L7/ContactsInfoViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class ContactsInfoViewController: UITableViewController {
     
-var persons : [Person] = []
+    var persons : [Person] = []
     
-
+    override func viewDidLoad() {
+        tableView.rowHeight = 80
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        persons[section].fullName
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellContactInfo", for: indexPath)
         
+        let person = persons[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = person.email
+        content.secondaryText = person.phoneNumber
+        
+        return cell
     }
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-
+}
